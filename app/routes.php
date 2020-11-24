5e38cb3ebb5e4be53f576fa19187e9c4b6f6ea28
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2020-11-24 17:20:20 +0100

    Create layouts for home and SMS-form pages.

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,4 @@
 /** Main routing file */
 
 require 'routes/homepage.php';
-require 'routes/login.php';
-require 'routes/register.php';
 require 'routes/sms-form.php';
--- app/routes/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-$app->get('/login', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Login page");
-    return $response;
-});
