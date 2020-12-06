668417faff89a05756eca85497a2db9c110b3212
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2020-12-06 18:57:03 +0100

    WIP: add validation for the sms form

--- ctec3110_private/app/routes/sms-form.php
+++ ctec3110_private/app/routes/sms-form.php
@@ -1,4 +1,6 @@
 <?php
+use Respect\Validation\Validator as v;
+
 use App\Models\Message;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -10,10 +12,29 @@ $app->get('/sms-form', function (Request $request, Response $response, $args) {
     return $view->render($response, 'sms-form.html.twig');
 });
 
+
+/**
+ * Validation docs: https://github.com/DavidePastore/Slim-Validation
+ */
+
+//Create the validators
+$numberValidator = v::numeric()->noWhitespace()->length(9, 12);
+$messageValidator = v::length(1,160);
+$validators = array(
+    'number' => $numberValidator,
+    'message' => $messageValidator
+);
+
 $app->post('/send-sms', function (Request $request, Response $response, $args) {
-    $message = new Message();
-    $message->destination_number = $request->getParsedBody()['phoneNumber'];
-    $message->value = $request->getParsedBody()['message'];
-    $message->save();
-    return $response->withStatus(302)->withHeader('Location', '/');
-});
+
+    if ($request->getAttribute('has_errors')) {
+        $errors = $request->getAttribute('errors');
+    } else {
+        $message = new Message();
+        $message->destination_number = $request->getParsedBody()['phoneNumber'];
+        $message->value = $request->getParsedBody()['message'];
+        $message->save();
+        return $response->withStatus(302)->withHeader('Location', '/');
+    }
+
+})->add(new \DavidePastore\Slim\Validation\Validation($validators));
