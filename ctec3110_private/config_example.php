9bba86f29f2bcd17620e4616c5babb9e64bbb3ca
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-12-06 16:43:23 +0000

    allow user to create an account

--- ctec3110_private/config_example.php
+++ ctec3110_private/config_example.php
@@ -1,7 +1,9 @@
 <?php
 /** Config file with database connection parameters. */
+define('DB_DRIVER', 'mysql');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'ctec3110');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_PORT', 3306);
+define('DB_CHARSET', 'utf8');
