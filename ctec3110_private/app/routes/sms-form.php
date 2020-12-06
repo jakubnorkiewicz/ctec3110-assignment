8f22c3fb31f0b9681e7ce21d6cf7adc0e8ab2ccc
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2020-12-06 18:40:05 +0100

    save data from the form in the DB

--- ctec3110_private/app/routes/sms-form.php
+++ ctec3110_private/app/routes/sms-form.php
@@ -1,10 +1,19 @@
 <?php
-
+use App\Models\Message;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
 
+
 $app->get('/sms-form', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'sms-form.html.twig');
+});
+
+$app->post('/send-sms', function (Request $request, Response $response, $args) {
+    $message = new Message();
+    $message->destination_number = $request->getParsedBody()['phoneNumber'];
+    $message->value = $request->getParsedBody()['message'];
+    $message->save();
+    return $response->withStatus(302)->withHeader('Location', '/');
 });
