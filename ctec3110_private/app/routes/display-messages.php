515e44642cc65145dee0569c2f912fe1942df6c0
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2020-12-07 15:03:04 +0000

    Download data from database and display it on the frontend

---
+++ ctec3110_private/app/routes/display-messages.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\ReceivedMessage;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+$app->get('/display', function (Request $request, Response $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'display-messages.html.twig', [
+        'received_messages' => ReceivedMessage::all(),
+    ]);
+});
