4951036741b9bf117bdc900161749a30920dbbac
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2020-12-08 01:21:28 +0100

    add validation to SMS form

--- ctec3110_private/app/routes/sms-form.php
+++ ctec3110_private/app/routes/sms-form.php
@@ -8,15 +8,28 @@ use Slim\Views\Twig;
 
 $app->get('/sms-form', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'sms-form.html.twig');
+    return $view->render($response, 'sms-form.html.twig', ['clean'=>true]);
 });
 
 $app->post('/send-sms', function (Request $request, Response $response, $args) {
 
-        $message = new SentMessage();
-        $message->destination_number = $request->getParsedBody()['phoneNumber'];
-        $message->value = $request->getParsedBody()['message'];
-        $message->save();
-        return $response->withStatus(302)->withHeader('Location', '/sms-form');
+        $view = Twig::fromRequest($request);
+        $v = new Valitron\Validator($_POST);
+        $v->rule('required', ['phoneNumber', 'message']);
+        $v->rule('integer', 'phoneNumber');
+        $v->rule('lengthBetween', 'phoneNumber', 9, 12);
+        $v->rule('lengthBetween', 'message', 1, 160);
 
+        if($v->validate()) {
+
+            $message = new SentMessage();
+            $message->destination_number = $request->getParsedBody()['phoneNumber'];
+            $message->value = $request->getParsedBody()['message'];
+            $message->save();
+            return $view->render($response, 'sms-form.html.twig', ['validationPassed' => true]);
+
+        } else {
+            return $view->render($response, 'sms-form.html.twig', ['validationPassed'=>false]);
+
+        }
 });
