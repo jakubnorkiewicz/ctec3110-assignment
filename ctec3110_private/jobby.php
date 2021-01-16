62f8e484eadb470347c1671ed4f2de28d7ba00a8
Jakub Norkiewicz <kubanorkiewicz@gmail.com>
2021-01-16 18:48:30 +0000

    jobby

--- ctec3110_private/jobby.php
+++ ctec3110_private/jobby.php
@@ -12,17 +12,47 @@ $jobby = new \Jobby\Jobby();
 
 $jobby->add('CommandExample', array(
     'command' => 'ls',
-    'schedule' => '0 * * * *',
+    'schedule' => '* * * * *',
     'output' => 'logs/command.log',
     'enabled' => true,
 ));
 
-$jobby->add('ClosureExample', array(
-    'command' => function() {
-        echo "I'm a function!\n";
+$jobby->add('SaveToDatabase', array(
+    'closure' => function() {
+        require_once __DIR__ . '/../vendor/autoload.php';
+        $soap_client_parameters = ['trace' => true, 'exceptions' => true];
+
+        // Initialize WS with the WSDL
+        $client = new SoapClient('https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl', $soap_client_parameters);
+
+        $paramsReadMessages = array(
+            "username" => '20_1719317',
+            "password" => 'Cotojestjanie723',
+            "count" => 10,
+            "deviceMSISDN" => "",
+        );
+
+        $results = $client->__soapCall("peekMessages", $paramsReadMessages);
+
+        foreach ($results as $result) {
+            $json = json_encode(simplexml_load_string($result));
+            $array = json_decode($json,true);
+
+            $message = new \App\Models\ReceivedMessage();
+            $message->source_number = $array['sourcemsisdm'];
+            $message->destination_number = $array['destinationmsisdn'];
+            $message->value = $array['message']['value'];
+            $message->bearer = $array['bearer'];
+            $message->message_ref = $array['messageref'];
+            $message->switch = $array['message']['iotdevice']['switches'];
+            $message->fan_fwd_or_rvs = $array['message']['iotdevice']['fan'];
+            $message->keypad_number = $array['message']['iotdevice']['keypad'];
+            $message->heater_temp = $array['message']['iotdevice']['temperature'];
+            $message->save();
+        }
         return true;
     },
-    'schedule' => '0 * * * *',
+    'schedule' => '* * * * *',
     'output' => 'logs/closure.log',
     'enabled' => true,
 ));
