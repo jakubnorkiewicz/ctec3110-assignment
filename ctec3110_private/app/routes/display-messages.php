60c8634eb5cfe608c02006fcc3506cc5b7213d70
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-01-17 23:34:29 +0000

    remove debugging code

--- ctec3110_private/app/routes/display-messages.php
+++ ctec3110_private/app/routes/display-messages.php
@@ -15,38 +15,7 @@ use Symfony\Component\VarDumper\VarDumper;
 $app->get('/display', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
 
-
-    // *** Duplicated from sms-form.php ***
-    $soap_client_parameters = ['trace' => true, 'exceptions' => true];
-
-    // Initialize WS with the WSDL
-    $client = new SoapClient($_ENV['WSDL_URL'], $soap_client_parameters);
-
-    $paramsReadMessages = array(
-        "username" => $_ENV['WSDL_USERNAME'],
-        "password" => $_ENV['WSDL_PASSWORD'],
-        "count" => 10,
-        "deviceMSISDN" => "",
-    );
-
-    // Invoke with the request params
-    $results = $client->__soapCall("peekMessages", $paramsReadMessages);
-    $messages = [];
-
-    foreach ($results as $result) {
-        $json = json_encode(simplexml_load_string($result));
-        $array = json_decode($json,true);
-        VarDumper::dump($array);
-    }
-
-
-//    $json = json_encode(simplexml_load_string($result));
-//    $array = json_decode($json,true);
-//    var_dump($array);
-//    echo '<br><br><br>';
-
     return $view->render($response, 'display-messages.html.twig', [
-//      'received_messages' => $result, TODO use the $result instead of the dummy data
         'received_messages' => ReceivedMessage::all(),
         'user' => $_SESSION['_sf2_attributes']['user'] ?? null
     ]);
